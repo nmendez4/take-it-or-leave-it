937a4beb5135eca0658d348787a866ae94a35a03
Hlaing Aung <betahba@gmail.com>
2022-01-05 19:59:24 -0500

    added dashboard and home routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,16 @@
 // index
+const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+
+
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
