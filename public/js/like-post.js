79acec39485044d3f8067b66060dca45d14ba6b5
nmendez4 <nmendez6594@gmail.com>
2022-01-05 20:33:19 -0500

    more js files

---
+++ public/js/like-post.js
@@ -0,0 +1,31 @@
+async function likesFormHandler(event) {
+    event.preventDefault();
+
+    const likes = document
+
+    const postId = window.location.toString().split('/')[
+        window.location.toString().split.length - 1
+    ];
+
+    if (likes) {
+        const response = await fetch(`/api/post/likes`, {
+            method: 'POST',
+            body: JSON.stringify({
+                likes,
+                postId
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText)
+        }
+    }
+}
+
+// missing selector
+document.querySelector('.').addEventListener('click', likesFormHandler);
