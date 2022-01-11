c7549a72a53590eb27193eb5a9b66379826306e2
nmendez4 <nmendez6594@gmail.com>
2022-01-10 20:43:41 -0500

    pushing

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    localhost,
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306
 });
