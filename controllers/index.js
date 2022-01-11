e84ab6585ca4d61d55bd34cb2f204e0b37297296
Hlaing Aung <betahba@gmail.com>
2022-01-11 16:05:03 -0500

    updated

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,3 @@
-// const router = require('express').Router();
-
-// const apiRoutes = require('./api');
-
-// router.use('/api', apiRoutes);
-
-// router.use((req, res) => {
-//     res.status(404).end();
-//   });
-  
-//   module.exports = router;
-
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
