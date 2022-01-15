fc52e04292e3528402567422b7308d63d8f08ed1
Hlaing Aung <betahba@gmail.com>
2022-01-15 14:40:54 -0500

    heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 
 const sess = {
@@ -39,5 +39,5 @@ app.use(session(sess));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(port, () => console.log('Now listening'));
 });
